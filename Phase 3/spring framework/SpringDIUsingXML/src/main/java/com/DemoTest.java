5ab78ef57df3d53cec1ff12e5fe67192e035b5cd
Akash <akash300383@gmail.com>
2023-06-04 23:51:46 +0530

    done

--- Phase 3/spring framework/SpringDIUsingXML/src/main/java/com/DemoTest.java
+++ Phase 3/spring framework/SpringDIUsingXML/src/main/java/com/DemoTest.java
@@ -1,11 +1,51 @@
 package com;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class DemoTest {
 
 	public static void main(String[] args) {
-		//Employee emp = new Employee();
+		//Employee emp1 = new Employee();
+		// Employee emp2 = new Employee();
 		//emp.display();
+	//BeanFactory is a interface 
 		
+	//BeanFactory obj = new XmlBeanFactory(new ClassPathResource("beans.xml"));	
+		Resource res = new ClassPathResource("beans.xml");
+		BeanFactory obj = new XmlBeanFactory(res);
+		
+//		Employee emp1 = (Employee)obj.getBean("emp1");  // pull the object from container 
+//		emp1.display();
+//		
+//		Employee emp2 = (Employee)obj.getBean("emp1");  // pull the object from container 
+//		emp2.display();
+//		
+//		Employee emp3 = (Employee)obj.getBean("emp2");  // pull the object from container 
+//		emp3.display();
+//		
+//		Employee emp4 = (Employee)obj.getBean("emp2");  // pull the object from container 
+//		emp4.display();
+		
+		Employee emp1 = (Employee)obj.getBean("emp1");
+		System.out.println(emp1);     // toString method called.
+		
+		Employee emp2 = (Employee)obj.getBean("emp1");
+		System.out.println(emp2);     // toString method called.
+		
+		Employee emp3 = (Employee)obj.getBean("emp3");
+		System.out.println(emp3);     // toString method called.
+		
+		Employee emp4 = (Employee)obj.getBean("emp4");
+		System.out.println(emp4);     // toString method called.
+		
+		Employee emp5 = (Employee)obj.getBean("emp5");
+		System.out.println(emp5);     // toString method called.
+		
+		emp5.setSalary(45000);
+		System.out.println(emp5);
 	}
 
 }
--- Phase 3/spring framework/SpringDIUsingXML/src/main/java/com/Employee.java
+++ Phase 3/spring framework/SpringDIUsingXML/src/main/java/com/Employee.java
@@ -1,8 +1,51 @@
 package com;
 
 public class Employee {
+private int id;
+private String name;
+private float salary;
+
+	public Employee() {
+		System.out.println("object created...");
+	}
+	
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 
 	public void display() {
 		System.out.println("Employee class display method");
 	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
 }
